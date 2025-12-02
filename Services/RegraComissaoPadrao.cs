58c854b4d910e78d8c6d56e9d7a38d792236e0ae
guhpissai <gustavopissai3d@gmail.com>
2025-12-01 21:36:14 -0300

    refactor(RegraComissaoPadrao): Refatora a classe removendo if else e criando novo metodo Arredondar

--- Services/RegraComissaoPadrao.cs
+++ Services/RegraComissaoPadrao.cs
@@ -5,15 +5,23 @@ namespace DesafioTarget.Services;
 
 public class RegraComissaoPadrao : IRegraComissao
 {
+  private const decimal MetaAlta = 500M;
+  private const decimal MetaBaixa = 100M;
+  private const decimal PercentualAlto = 0.05M;
+  private const decimal PercentualBaixo = 0.01M;
   public decimal Calcular(Venda venda)
   {
-    decimal comissao = 0;
+    if (venda.Valor >= MetaAlta)
+      return Arredondar(venda.Valor * PercentualAlto);
 
-    if (venda.Valor >= 500)
-      comissao = venda.Valor * 0.05M;
-    else if (venda.Valor >= 100)
-      comissao = venda.Valor * 0.01M;
+    if (venda.Valor >= MetaBaixa)
+      return Arredondar(venda.Valor * PercentualBaixo);
 
-    return Math.Round(comissao, 2, MidpointRounding.AwayFromZero);
+    return 0;
+  }
+
+  private static decimal Arredondar(decimal valor)
+  {
+    return Math.Round(valor, 2, MidpointRounding.AwayFromZero);
   }
 }
