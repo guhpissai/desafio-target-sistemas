c2493ce1020d7b70e8723dc65f39fa65f321a763
guhpissai <gustavopissai3d@gmail.com>
2025-12-01 21:12:00 -0300

    refactor(JsonHelper): Ajusta os metodos para utilizarem stream

--- Helpers/JsonHelper.cs
+++ Helpers/JsonHelper.cs
@@ -1,24 +1,39 @@
 using System.Text.Json;
-using DesafioTarget.Models;
 
 namespace DesafioTarget.Helpers;
 
 public static class JsonHelper
 {
-  public static T Deserialize<T>(string path)
+  private static readonly JsonSerializerOptions _options = new()
   {
-    var json = File.ReadAllText(path);
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true
+  };
 
-    var options = new JsonSerializerOptions
+  public static T Deserialize<T>(string path) where T : new()
+  {
+    if (!File.Exists(path))
+      return new T();
+
+    try
     {
-      PropertyNameCaseInsensitive = true
-    };
+      using FileStream stream = File.OpenRead(path);
 
-    return JsonSerializer.Deserialize<T>(json, options)!;
+      return JsonSerializer.Deserialize<T>(stream, _options) ?? new T();
+    }
+    catch (JsonException)
+    {
+      return new T();
+    }
   }
 
-  public static void SaveChanges(object data, string path)
+  public static void SaveChanges<T>(T data, string path)
   {
-    File.WriteAllText(path, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true }));
+    var directory = Path.GetDirectoryName(path);
+    if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+      Directory.CreateDirectory(directory);
+
+    using FileStream stream = File.Create(path);
+    JsonSerializer.Serialize(stream, data, _options);
   }
 }
